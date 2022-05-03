79be276ce4088157ebfd886a8467df71f46b01ee
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-03 10:59:36 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,7 @@
+# desafio
+# Faça um programa que leia um num inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n1 = int(input('Digite um numero'))
+suc = n1 + 1
+ant = n1 - 1
+print('O numero é: {}, sucessor é: {}, antecessor é: {}'.format(n1, suc, ant))
