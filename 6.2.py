22fbb99da09781082824c776bf0bd0ab3e451d64
Ellen <ellen.code13@gmail.com>
2022-05-02 22:17:57 -0300

    Create 6.2.py

---
+++ 6.2.py
@@ -0,0 +1,10 @@
+# desafio
+
+# Faça um programa que leia algo pelo teclado e o seu tipo primitivo e todas as informações possíveis sobre ele.
+n1 = int(input('Digite um número: '))
+n2 = str(input('Digite algo: '))
+
+print('Foi digitado  {}, e foi digitado {}'.format(n1, n2))
+
+print(type(n1))
+print(type(n2))
