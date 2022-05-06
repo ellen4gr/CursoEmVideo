bd49fe15a049af131fb25794cdd4f30f969cf5db
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-06 08:32:21 -0300

    Add files via upload

---
+++ ManipulandoTexto.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.
+num = int(input('Digite um numero: '))
+n = str(num)
+print('{}'.format(n[3]))
+print('{}'.format(n[2]))
+print('{}'.format(n[1]))
+print('{}'.format(n[0]))
