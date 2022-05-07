cecc1a88b696599326917618a6c4a2c9a8c2404a
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-07 16:26:44 -0300

    Add files via upload

---
+++ CalculandoValorPassgem.py
@@ -0,0 +1,9 @@
+"""Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem cobrando
+R$ 0,50 poe km para viagens de até 200 km e R$ 0,45 para viagens mais longas."""
+distancia = float(input("Quantos km de distância? "))
+if distancia <= 200:
+    valorPassagem = (distancia * 0.50)
+    print('O valor será R$ {:.2f}'.format(valorPassagem))
+else:
+    valorPassagem = (distancia * 0.45)
+    print('O valor será R$ {:.2f}'.format(valorPassagem))
