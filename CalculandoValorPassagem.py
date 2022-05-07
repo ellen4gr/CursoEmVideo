3b8ee0b5d72e9c716d64793777162b1af55bdf11
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-07 17:03:42 -0300

    Update CalculandoValorPassagem.py

--- CalculandoValorPassagem.py
+++ CalculandoValorPassagem.py
@@ -3,7 +3,7 @@ R$ 0,50 poe km para viagens de até 200 km e R$ 0,45 para viagens mais longas.""
 distancia = float(input("Quantos km de distância? "))
 if distancia <= 200:
     valorPassagem = (distancia * 0.50)
-    print('O valor será R$ {:.2f}'.format(valorPassagem))
+    
 else:
     valorPassagem = (distancia * 0.45)
-    print('O valor será R$ {:.2f}'.format(valorPassagem))
+print('O valor será R$ {:.2f}'.format(valorPassagem))
