374749f64bc2214aeaf8f1581a0a32686500d23a
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-07 16:26:12 -0300

    Add files via upload

---
+++ ImparPar.py
@@ -0,0 +1,7 @@
+"""Crie um programa que leia um numero inteiro e mostre na tela se ele é PAR ou IMPAR."""
+
+num = (int(input("Digite um numero: ")))
+if (num % 2 == 0):
+    print('O número é par')
+else:
+    print(' O número digitado é impar')
