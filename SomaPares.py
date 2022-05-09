999d3c98f5cb1fa51f2f44784ba2ba304b48e389
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-09 20:55:26 -0300

    Update SomaPares.py

--- SomaPares.py
+++ SomaPares.py
@@ -1,8 +1,10 @@
 """ Crie um programa que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
 digitado pro ímpar desconsiderar.  """
 SomaPar = 0
-for i in range(6):
-    num = int(input('Digite o número: '))
+cont = 0
+for i in range(1, 7): # 
+    num = int(input('Digite o {} número: '.format(i)))
     if num % 2 == 0:
-        SomaPar = SomaPar + num
-print('\nA soma dos pares {}'.format(SomaPar))
+        SomaPar += num
+        cont += 1
+print('\nVocê digitou {} pares e a soma dos pares {}'.format(cont, SomaPar))
