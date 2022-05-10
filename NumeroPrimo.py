845a8c26c6399b9bdfdc1edf3c45e6d9a824929b
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-09 21:22:08 -0300

    Add files via upload

---
+++ NumeroPrimo.py
@@ -0,0 +1,6 @@
+"""Faça um programa que leia um número inteiro e diga se ele é ou não um número primo """
+num = int(input(' Digite um número inteiro: '))
+if num % 2 == 1:
+    print("O número é primo")
+else:
+    print("O número não primo")
