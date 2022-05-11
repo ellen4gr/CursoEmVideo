c62f7a4375b3ab6c56583b717b872f88e4bcd3ac
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-10 21:56:49 -0300

    Add files via upload

---
+++ Peso.py
@@ -0,0 +1,17 @@
+""" Faça um programa que leia o peso de cinco pessoas. No final mostre qual foi o maior e o menor peso lido. """
+
+
+maior = 0
+menor = 0
+for i in range(1, 6):
+    peso = float(input('Qual o peso da {} pessoa? '.format(i)))
+    if i == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}Kg'.format(maior))
+print('O menor peso lido foi de {}Kg'.format(menor))
